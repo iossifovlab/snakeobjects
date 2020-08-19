58198d742098aac63e2e6ed7157ecae03f08d6a0
Ivan Iossifov <iossifov@cshl.edu>
2020-08-19 13:25:18 -0400

    cleaned a bit the test_out.txt format

--- demo/d7/run.sh
+++ demo/d7/run.sh
@@ -1,23 +1,37 @@
 . setenv.sh
-cp P1 P.snakefile
-./build_object_graph.py createDirs
-run_snake.sh -j -R 
-ls -ltr objLinks/P/* >test_out.txt
 
-echo "test dependency of obj.flag on new target" >>test_out.txt
-cp P2 P.snakefile
-run_snake.sh -j -R P_c 
-ls -ltr objLinks/P/*  >>test_out.txt
+rm test_out.txt
 
+echo "BASELINE" >>test_out.txt
 ./clean.sh
 cp P1 P.snakefile
 ./build_object_graph.py createDirs
 run_snake.sh -j -R 
-ls -ltr objLinks/P/*  >>test_out.txt
+ls -tr objLinks/P/*  | grep -v log >>test_out.txt
+echo  >> test_out.txt
+echo  >> test_out.txt
+
+
+echo "test dependency of obj.flag on new target" >>test_out.txt
+cp P2 P.snakefile
+run_snake.sh -j -R P_c 
+ls -tr objLinks/P/*  | grep -v log >>test_out.txt
+echo  >> test_out.txt
+echo  >> test_out.txt
+
+
+echo "BASELINE" >>test_out.txt
+./clean.sh
+cp P1 P.snakefile
+./build_object_graph.py createDirs
+run_snake.sh -j -R 
+ls -tr objLinks/P/*  | grep -v log >>test_out.txt
+echo  >> test_out.txt
+echo  >> test_out.txt
 
 echo "test dependency of old target on the new one"  >>test_out.txt
 cp P3 P.snakefile
 run_snake.sh -j -R P_c 
-ls -ltr objLinks/P/*  >>test_out.txt
+ls -tr objLinks/P/*  | grep -v log >>test_out.txt
 
-
+diff test_out.txt correct_test_out.txt
