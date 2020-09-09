85ac11eadbf037f2a60b6363a0708ce1d8ed8bdf
Boris Yamrom <byamrom@mumdel.c.nygenome.org>
2020-09-09 14:44:21 -0400

    improved bin/submit_snake.sh and modified demo/d1/parameters.yaml and demo/d1/run.sh

--- bin/submit_snake.sh
+++ bin/submit_snake.sh
@@ -20,18 +20,46 @@ if [ ! -f header.snakefile ]; then
     iippl header.snakefile > $PROJECT_DIR/header.snakefile
 fi
 
-cmd=`grep -P "^cluster" ${PROJECT_DIR}/parameters.yaml |cut -d':' -f2`
-echo $cmd
 default_args=`grep -P "^default_snakemake_args" ${PROJECT_DIR}/parameters.yaml |cut -d':' -f2` 
 
-if [ -z "$cmd" ]; then
-    echo "run_snake.sh -j1 $*"
-	run_snake.sh -j1 $* 2> tmp &
+input=($default_args)
+
+N=`echo ${input[@]}|wc -w`
+profile=""
+for n in `seq 1  $N`
+do
+	if [[ ${input[$n]} ==  "--profile" ]]; then
+		let k=$n+1
+		profile=${input[$k]}
+	fi
+done
+
+if [ -z $profile ]; then
+	echo "no profile specified"
+	exit 1
 else
-	echo "$cmd $PROJECT_DIR/jscript.sh $default_args $* 2>tmp &"
-	$cmd jscript.sh $default_args $* 2> tmp &
+	echo $profile
 fi
 
+cmd=""
 
+if [ ! -f $HOME/.config/snakemake/$profile/config.yaml ]; then
+	echo "no config.yaml in the profile $HOME/.config/snakemake/$profile"
+	if [ ! -f $profile/config.yaml ]; then
+		echo "no config.yaml in the profile $profile"
+		exit 1
+	else
+		cmd=`grep -P "^cluster\:" $profile/config.yaml|cut -d':' -f2`
+    fi
+else
+cmd=`grep -P "^cluster\:" $HOME/.config/snakemake/$profile/config.yaml|cut -d':' -f -2`
+fi
 
+if [ -z "$cmd" ]; then
+    echo "no cluster specified in profile config.yaml file"
+	exit 1
+else
+	echo "$cmd $PROJECT_DIR/jscript.sh $default_args $* 2>tmp &"
+	$cmd jscript.sh $default_args $*  &
+fi
 
