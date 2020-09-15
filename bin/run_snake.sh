b2de441d0e994628898b3b25f9d8e0dced534093
Ivan Iossifov <iossifov@cshl.edu>
2020-09-15 15:32:47 -0400

    Make it work with SGE.wigclust.

--- bin/run_snake.sh
+++ bin/run_snake.sh
@@ -11,20 +11,10 @@ fi
 echo "WORKING ON PROJECT" $PROJECT_DIR 
 echo "WITH PIPELINE" $PIPELINE_DIR
 
-mkdir -p $PROJECT_DIR/objLinks
 cd $PROJECT_DIR/objLinks
-#mkdir -p log
 
-default_options=`grep -P "^default_snakemake_args" ${PROJECT_DIR}/parameters.yaml |cut -d':' -f2`
-
-if [ -z "$default_options" ]; then
-
-echo "snakemake --snakefile .pipes/main.snakefile  $*"
-snakemake --snakefile .pipes/main.snakefile  $*
-else
-
+if [ -f "${PROJECT_DIR}/parameters.yaml" ]; then
+    default_options=`grep -P "^default_snakemake_args" ${PROJECT_DIR}/parameters.yaml |cut -d':' -f2`
+fi
 echo "snakemake --snakefile .pipes/main.snakefile  $default_options $*"
 snakemake --snakefile .pipes/main.snakefile  $default_options $*
-fi
-
-# --profile $BIN_DIR/SLURM.nygc $*
