57098e1c34833c3b0196ff0e2f2dd41946b6789e
Boris Yamrom <yamrom@cshl.edu>
2021-01-29 11:29:34 -0500

    remiving slurm-nygc is added

--- tests/demos/d5/clean.sh
+++ tests/demos/d5/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname "$0")
-rm -rf objects/.snakemake OG.json OG.OG glbl.makefile log objects base jobscript.sh
+rm -rf slurm-nygc objects/.snakemake OG.json OG.OG glbl.makefile log objects base jobscript.sh
 
