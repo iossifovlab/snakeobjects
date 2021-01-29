29c165aa0e80c5b807300374a6112f703e6a8d03
Boris Yamrom <yamrom@cshl.edu>
2021-01-29 11:25:20 -0500

    reference to PROJECT_DIR replaced with SO_PROJECT

--- snakeobjects/jobscript.sh
+++ snakeobjects/jobscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # properties = {"type": "single", "rule": "run_snake", "input": {}, "output":{},"params":{}, "log":{}, "wildcards": {}, "local": false, "threads": 1, "resources": {"mem": 8000}, "cluster": {}}
- cd $PROJECT_DIR/objects && \
+ cd $SO_PROJECT/objects && \
 python \
--m snakemake --snakefile $PROJECT_DIR/objects/.snakeobjects/main.snakefile \
+-m snakemake --snakefile $SO_PROJECT/objects/.snakeobjects/main.snakefile \
 --force -j --keep-target-files --keep-remote \
 --attempt 1 --force-use-threads \
 --wrapper-prefix https://github.com/snakemake/snakemake-wrappers/raw/ \
