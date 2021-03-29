1f37a0202c7800bbb8d108e09d302b399a143eca
Boris Yamrom <yamrom@zermatt.home>
2021-03-29 15:54:18 -0400

    changed path

--- documentation/conf.py
+++ documentation/conf.py
@@ -13,8 +13,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../snakeobjects'))
-
+#sys.path.insert(0, os.path.abspath('../snakeobjects'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 # -- Project information -----------------------------------------------------
 
 project = 'snakeobjects'
