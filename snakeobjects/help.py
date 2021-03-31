72a6a99476758b56423b3de3b9a925a8a8752a8e
Boris Yamrom <yamrom@zermatt.home>
2021-03-31 15:45:52 -0400

    added importlib import

--- snakeobjects/help.py
+++ snakeobjects/help.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+import importlib.resources as importlib_resources
 import os,sys
+from importlib.util import spec_from_file_location, module_from_spec
 
 helpData = {
     "version": "prints the version",
@@ -70,7 +71,7 @@ def help(args=None):
     command = args[0]
 
     if command in ["help", "-h", "--help"]:
-        #print("Snakeobjects %s\n" % (__version__))
+        print("Snakeobjects %s\n" % (__version__))
         if len(args) == 1:
             print("Available commands are:\n\t", "\n\t".join(helpData.keys()),"\n",sep="")
         # print("Typical sequence of commands is descripe, prepareTest, prepare, run:\n")
