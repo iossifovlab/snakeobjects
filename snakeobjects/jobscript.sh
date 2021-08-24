e37a98feaaded614ee56a3a9d707186cbae9e003
Boris Yamrom <yamrom@cshl.edu>
2021-08-24 11:25:49 -0400

    imodified jobschript.sh to match new directory structure

--- snakeobjects/jobscript.sh
+++ snakeobjects/jobscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # properties = {"type": "single", "rule": "run_snake", "input": {}, "output":{},"params":{}, "log":{}, "wildcards": {}, "local": false, "threads": 1, "resources": {"mem": 64000}, "cluster": {}}
- cd $SO_PROJECT/objects && \
+ cd $SO_PROJECT && \
 python \
--m snakemake --snakefile $SO_PROJECT/objects/.snakeobjects/main.snakefile \
+-m snakemake --snakefile $SO_PIPELINE/Snakefile \
 --force -j --keep-target-files --keep-remote \
 --attempt 1 --force-use-threads \
 --wrapper-prefix https://github.com/snakemake/snakemake-wrappers/raw/ \
