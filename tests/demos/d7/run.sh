4b65e0e42b4cee9ad156b46916e96f8688fd04ed
Boris Yamrom <yamrom@cshl.edu>
2021-09-05 09:17:45 -0400

    made changes to pass pytest with the lates write_main_snakefile

--- tests/demos/d7/run.sh
+++ tests/demos/d7/run.sh
@@ -6,7 +6,7 @@ rm -f test_out.txt
 
 echo "BASELINE" >>test_out.txt
 ./clean.sh
-cp P1.snakefile P.snakefile
+cp P1.snakefile.sav P.snakefile
 sobjects prepare
 sobjects run -j -R 
 ls P/*  | grep -v log >>test_out.txt
@@ -15,7 +15,7 @@ echo  >> test_out.txt
 
 
 echo "test dependency of obj.flag on new target" >>test_out.txt
-cp P2.snakefile P.snakefile
+cp P2.snakefile.sav P.snakefile
 sobjects run -j -R P_c 
 ls P/*  | grep -v log >>test_out.txt
 echo  >> test_out.txt
@@ -24,7 +24,7 @@ echo  >> test_out.txt
 
 echo "BASELINE" >>test_out.txt
 ./clean.sh
-cp P1.snakefile P.snakefile
+cp P1.snakefile.sav P.snakefile
 sobjects prepare
 sobjects run -j -R 
 ls P/*  | grep -v log >>test_out.txt
@@ -32,7 +32,7 @@ echo  >> test_out.txt
 echo  >> test_out.txt
 
 echo "test dependency of old target on the new one"  >>test_out.txt
-cp P3.snakefile P.snakefile
+cp P3.snakefile.sav P.snakefile
 sobjects run -j -R P_c 
 ls P/*  | grep -v log >>test_out.txt
 
