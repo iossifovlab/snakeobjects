8a200d425193fde8f239ce66eb945a475a60b3ce
Ivan Iossifov <iossifov@cshl.edu>
2022-11-29 18:16:52 -0500

    Polished the python scripts in the paper examples.

--- documentation/snakeobjectsPaperExample/pipeline/build_object_graph.py
+++ documentation/snakeobjectsPaperExample/pipeline/build_object_graph.py
@@ -1,15 +1,18 @@
 import pandas as pd
-def run(project, OG):
-  PED = pd.read_table(project.parameters['pedigree'], 
-                      sep='\t', header=0)
-  OG.add('reference','hg38')
-  for i, r in PED.iterrows():
-    OG.add('individual',r['personId'], 
-               {'fqId':r['fastqId']},OG['reference'])
-  for i, r in PED.iterrows():
-    if r['fatherId'] == '.' or r['motherId'] == '.': 
-       continue
-    OG.add('trio',r['personId'], {},
-           [OG['individual',r[i]] 
-            for i in ['fatherId', 'motherId','personId']])
-  OG.add('denovos','all', {}, OG['trio'])
+from snakeobjects import Project, ObjectGraph
+
+
+def run(project: Project, OG: ObjectGraph):
+    PED = pd.read_table(project.parameters['pedigree'],
+                        sep='\t', header=0)
+    OG.add('reference', 'hg38')
+    for _, r in PED.iterrows():
+        OG.add('individual', r['personId'],
+                   {'fqId': r['fastqId']}, OG['reference'])
+    for _, r in PED.iterrows():
+        if r['fatherId'] == '.' or r['motherId'] == '.':
+            continue
+        OG.add('trio', r['personId'], {},
+               [OG['individual', r[a]]
+                for a in ['fatherId', 'motherId', 'personId']])
+    OG.add('denovos', 'all', {}, OG['trio'])
